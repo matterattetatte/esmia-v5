bca61f7d3aa7303ef3066a98b2e56554498fb185
Mattz <matias.marti@medieinstitutet.com>
2025-11-22 00:43:10 +0100

    improve seo and other best practices

--- eleventy.config.js
+++ eleventy.config.js
@@ -25,6 +25,12 @@ export default async function(eleventyConfig) {
 			"./public/": "/"
 		})
 
+
+    eleventyConfig.addPassthroughCopy({
+    "content/robots.txt": "robots.txt"
+  });
+
+
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
 
